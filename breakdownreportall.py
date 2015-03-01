ed2ec6bed4238c7d2332b2e6cd1b9ee5aaab3419
Daniel Del Rio <danny@ens-labs.com>
2015-03-01 10:37:53 -0400

    Changed from breakdowns to averias

--- breakdownreportall.py
+++ breakdownreportall.py
@@ -5,12 +5,12 @@ aee_client = Client(aee_url)
 towns = aee_client.service.getBreakdownsSummary()
 
 for town in towns:
-    breakdowns = aee_client.service.getBreakdownsByTownOrCity(town.r1TownOrCity)
-    for breakdown in breakdowns: 
+    averias = aee_client.service.getBreakdownsByTownOrCity(town.r1TownOrCity)
+    for averia in averias: 
     	print "***************************************"    
-        print "Pueblo: " + breakdown.r1TownOrCity
-        print "Area: " + breakdown.r2Area
-        print "Status: " + breakdown.r3Status
-        print "Last Update: " + breakdown.r4LastUpdate
+        print "Pueblo: " + averia.r1TownOrCity
+        print "Area: " + averia.r2Area
+        print "Status: " + averia.r3Status
+        print "Last Update: " + averia.r4LastUpdate
         print "***************************************"
 
