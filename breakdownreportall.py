4bf0ce482f74228eae948e5857ec55cc7a88c04f
ens-labs <danny@ens-labs.com>
2015-06-24 15:56:48 -0400

    Translated reportall

--- breakdownreportall.py
+++ breakdownreportall.py
@@ -5,11 +5,11 @@ aee_client = Client(aee_url)
 towns = aee_client.service.getBreakdownsSummary()
 
 for town in towns:
-    averias = aee_client.service.getBreakdownsByTownOrCity(town.r1TownOrCity)
-    for averia in averias: 
+    breakdowns = aee_client.service.getBreakdownsByTownOrCity(town.r1TownOrCity)
+    for breakdown in breakdowns: 
     	print "***************************************"    
-        print "Pueblo: " + averia.r1TownOrCity
-        print "Area: " + averia.r2Area
-        print "Status: " + averia.r3Status
-        print "Last Update: " + averia.r4LastUpdate
+        print "Pueblo: " + breakdown.r1TownOrCity
+        print "Area: " + breakdown.r2Area
+        print "Status: " + breakdown.r3Status
+        print "Last Update: " + breakdown.r4LastUpdate
         print "***************************************"
